07110f6b0e6ed9161ee7d7a763133995bb8929d6
henryzada1 <diego.nasciment@hotmail.com>
2022-06-02 11:58:04 -0300

    Primeiro App

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+    <View style={estilo.container}>
+      <Text style={estilo.titulo}>Álbuns do milênio</Text>
+      <ScrollView>
       <StatusBar style="auto" />
+      <Text style={estilo.album}>Blurryface</Text>
+      <Image style={estilo.img}source={require("./assets/blurryface.jpg")}/>
+      <Text style={estilo.album}>Eyes Wide Open</Text>
+      <Image style={estilo.img}source={require("./assets/eyeswideopen.jpg")}/>
+      <Text style={estilo.album}>Formula of Love</Text>
+      <Image style={estilo.img}source={require("./assets/formulaoflove.jpg")}/>
+      <Text style={estilo.album}>Scaled & Icy</Text>
+      <Image style={estilo.img}source={require("./assets/scalednicy.jpg")}/>
+
+      </ScrollView>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8eces4',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  titulo:{
+      fontSize:40,
+      marginTop:30,
+      color:"#023947",
+  },
+  img:{
+    width:300,
+    height:300,
+    marginTop:2,
+    borderRadius:10,
+  },
+  album:{
+    fontSize:20,
+    marginTop:40,
+    marginBottom:8,
+    textAlign:'center',
+  }
 });
